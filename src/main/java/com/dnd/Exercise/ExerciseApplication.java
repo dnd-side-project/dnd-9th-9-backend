0886def276a149c01c9c5bcb9a58a62fd791f134
Ju-yeop <dhwnduq12@naver.com>
2023-07-18 19:45:26 +0900

    Setting: 프로젝트 세팅

---
+++ src/main/java/com/dnd/Exercise/ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseApplication.class, args);
+	}
+
+}
