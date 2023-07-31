eb5480a20fae60bca93fb54ab57e2fa5557659bf
Ju-yeop <dhwnduq12@naver.com>
2023-08-01 01:00:01 +0900

    Feat: BaseEntity 추가 (#6) - ExerciseApplication 클래스 - @EnableJpaAuditing 추가

--- src/main/java/com/dnd/Exercise/ExerciseApplication.java
+++ src/main/java/com/dnd/Exercise/ExerciseApplication.java
@@ -2,8 +2,10 @@ package com.dnd.Exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
