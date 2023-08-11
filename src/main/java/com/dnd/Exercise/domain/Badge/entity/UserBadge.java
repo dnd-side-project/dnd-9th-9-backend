9623af5bfaee8fb419692e2982e64e2033c6c831
Jimin <kaley0421@naver.com>
2023-08-12 00:35:31 +0900

    Feat: UserBadge Entity 추가 (#12)

---
+++ src/main/java/com/dnd/Exercise/domain/Badge/entity/UserBadge.java
@@ -0,0 +1,31 @@
+package com.dnd.Exercise.domain.Badge.entity;
+
+import com.dnd.Exercise.domain.match.entity.Match;
+import com.dnd.Exercise.domain.user.entity.User;
+import com.dnd.Exercise.global.common.BaseEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+public class UserBadge extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_badge_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "match_id")
+    private Match match;
+}
