2fe88f374cd2d25693e1aa83b4ea6194981e6198
Ju-yeop <dhwnduq12@naver.com>
2023-08-15 02:26:06 +0900

    Feat: JPAQueryFactory Bean 등록 (#27)

--- src/main/java/com/dnd/Exercise/global/config/WebConfig.java
+++ src/main/java/com/dnd/Exercise/global/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.dnd.Exercise.global.config;
 
 import com.dnd.Exercise.global.slack.RequestStorage;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -9,6 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 
     @Bean
     @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
