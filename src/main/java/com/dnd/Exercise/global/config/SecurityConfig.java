4be9d06d8bcbe98c8a47f305c9c68165a15d00bb
Jimin <kaley0421@naver.com>
2023-08-16 22:26:53 +0900

    Feat: passwordEncoder 빈 추가 (#31)

--- src/main/java/com/dnd/Exercise/global/config/SecurityConfig.java
+++ src/main/java/com/dnd/Exercise/global/config/SecurityConfig.java
@@ -5,12 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
