7be6a5b33c482ceea1a950d33930ba3633887c19
Jimin <kaley0421@naver.com>
2023-08-17 06:36:26 +0900

    Feat: JWT 세팅 (#31)

---
+++ src/main/java/com/dnd/Exercise/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,86 @@
+package com.dnd.Exercise.global.jwt;
+
+import com.dnd.Exercise.domain.auth.service.CustomUserDetailsService;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    // TODO: refresh 토큰 발급 추가
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-valid-minute}")
+    private long tokenValidTime;
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        tokenValidTime = tokenValidTime * 60 * 1000L;
+    }
+
+    public String createToken(String uid) {
+        Claims claims = Jwts.claims().setSubject(uid);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserUid(token));
+        return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, AuthorityUtils.NO_AUTHORITIES);
+    }
+
+    public String getUserUid(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            return null;
+        }
+        return authorization.substring("Bearer ".length());
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (SignatureException | MalformedJwtException e) {
+            log.info("JWT 토큰이 유효하지 않습니다.");
+            // TODO: 예외처리
+        } catch (ExpiredJwtException e) {
+            log.info("JWT 토큰이 만료되었습니다.");
+            // TODO: 예외처리
+        } catch (UnsupportedJwtException e) {
+            log.info("지원하지 않는 JWT 토큰입니다.");
+            // TODO: 예외처리
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+}
