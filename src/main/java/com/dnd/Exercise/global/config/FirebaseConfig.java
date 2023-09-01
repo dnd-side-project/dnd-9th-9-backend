f7f37500895a281b3931213c963e834836e3ec2a
Ju-yeop <dhwnduq12@naver.com>
2023-09-01 22:40:54 +0900

    Feat: firebase 세팅 (#64) - FirebaseConfig 파일 추가

---
+++ src/main/java/com/dnd/Exercise/global/config/FirebaseConfig.java
@@ -0,0 +1,40 @@
+package com.dnd.Exercise.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class FirebaseConfig {
+    @Value("classpath:firebase/firebase_service_key.json")
+    private Resource resource;
+
+    @PostConstruct
+    public FirebaseApp initFireBase(){
+        try {
+            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            return FirebaseApp.initializeApp(options);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(){
+        FirebaseMessaging instance = FirebaseMessaging.getInstance();
+        return instance;
+    }
+
+}
