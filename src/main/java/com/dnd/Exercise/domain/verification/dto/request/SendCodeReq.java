65193fc855341006615757df91a50e99dd5b9a00
Jimin <kaley0421@naver.com>
2023-09-10 19:51:35 +0900

    Feat: 휴대폰번호, 인증코드번호 정규식 validation 추가 (#72)

--- src/main/java/com/dnd/Exercise/domain/verification/dto/request/SendCodeReq.java
+++ src/main/java/com/dnd/Exercise/domain/verification/dto/request/SendCodeReq.java
@@ -1,13 +1,17 @@
 package com.dnd.Exercise.domain.verification.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor
 public class SendCodeReq {
+    @ApiModelProperty(value = "10~11 자리 숫자의 휴대폰번호", required = true, example = "01012345678")
     @NotBlank
+    @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$", message = "휴대폰 번호는 10~11자리의 숫자로만 입력 가능합니다.")
     private String phoneNum;
 }
