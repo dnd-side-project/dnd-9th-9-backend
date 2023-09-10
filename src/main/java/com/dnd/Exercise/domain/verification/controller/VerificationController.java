33bea5a964031781a01fe9186b98f2c843d9d7b6
Jimin <kaley0421@naver.com>
2023-09-10 18:54:30 +0900

    Docs: Verification API swagger 설명 추가 (#72)

--- src/main/java/com/dnd/Exercise/domain/verification/controller/VerificationController.java
+++ src/main/java/com/dnd/Exercise/domain/verification/controller/VerificationController.java
@@ -6,6 +6,8 @@ import com.dnd.Exercise.domain.verification.service.VerificationService;
 import com.dnd.Exercise.global.common.ResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +28,9 @@ public class VerificationController {
     private final VerificationService verificationService;
 
     @ApiOperation(value = "인증번호 전송 요청 📞", notes = "해당 전화번호로 인증번호를 발송합니다.")
+    @ApiResponses({
+            @ApiResponse(code=200, message="인증번호 발송 완료")
+    })
     @PostMapping("/send-code")
     public ResponseEntity<String> sendCode(@RequestBody @Valid SendCodeReq sendCodeReq) {
         try {
@@ -36,7 +41,11 @@ public class VerificationController {
         return ResponseDto.ok("인증번호 발송 완료");
     }
 
-    @ApiOperation(value = "인증번호 인증 📞", notes = "발송받은 인증번호로 인증을 수행합니다. 인증 성공 여부를 반환합니다.")
+    @ApiOperation(value = "인증번호 인증 📞", notes = "발송받은 인증번호로 인증을 수행합니다.")
+    @ApiResponses({
+            @ApiResponse(code=200, message="인증되었습니다."),
+            @ApiResponse(code=400, message="잘못된 인증번호입니다. or 인증번호 유효시간이 지났습니다.")
+    })
     @PostMapping("/verify")
     public ResponseEntity<String> verify(@RequestBody @Valid VerifyReq verifyReq) {
         verificationService.verify(verifyReq);
